5808cd3e4e5a4ff6b3f35858c73a3e512b86d785
Bastian Stender <basti@randomprojects.de>
2018-12-27 16:21:37 +0100

    Initial commit
    Add a flask server application serving a very basic HTTP API for:
    
      - creating/modifying/viewing accounts
      - adding/view money (transactions)
      - performing payments
    
    Signed-off-by: Bastian Stender <basti@randomprojects.de>

---
+++ README.rst
@@ -0,0 +1,93 @@
+|python3.5| |python3.6| |python3.7| |build-status| |lgtm-alerts| |lgtm-grade|
+
+Prepaid Mate
+============
+
+Tired of fishing cash out of your pockets? Regardless of whether you pronounce
+it [ˈpriːpɛɪ̯t ˈmaːtə] or [ˈpriːpɛɪ̯t meɪt], paying for drinks was never easier!
+
+Put a fistful of dollars in the piggy bank and add the amount using any
+computer or smartphone to your account. Thirsty? Start scanning! Identify
+yourself with your personal RFID token (or barcode) and scan the barcode on the
+drink. Cheers!
+
+Required Hardware
+=================
+
+* some (embedded) computer running the software
+* USB HID barcode scanner
+* optional: USB HID RFID scanner
+* optional: speakers for audio feedback
+
+Software Components
+===================
+
+* flask server application providing a simple sqlite-backed API for
+  - creating/modifying/viewing accounts
+  - adding/view money (transactions)
+  - performing payments
+* scanner client listening to barcode/RFID events and triggering payments
+* simple, static web frontend (currently in development)
+
+Run it
+======
+
+Assuming you're running Debian (or Ubuntu) and want to use a virtualenv:
+
+.. code-block:: bash
+
+    $ apt-get install python3 python3-virtualenv python3-pip virtualenv
+
+Create the virtualenv and activate it:
+
+.. code-block:: bash
+
+    $ virtualenv -p python3 prepaid-drinks-venv
+    $ source prepaid-drinks-venv/bin/activate
+
+Now install the Python dependencies via pip inside the virtualenv:
+
+.. code-block:: bash
+
+    (prepaid-drinks-venv) $ pip install -r requirements.txt
+
+Create a config file named `config`. Use `config.sample` as a starting point.
+
+Now start the development server:
+
+.. code-block:: bash
+
+    (prepaid-drinks-venv) $ flask run
+
+Or start the development server in debug mode:
+
+.. code-block:: bash
+
+    (prepaid-drinks-venv) $ FLASK_DEBUG=1 flask run
+
+Now head your browser to `http://localhost:5000/static/index.html` to start.
+Do not use the development server in a production environment.
+
+.. |python3.5| image:: https://img.shields.io/badge/python-3.5-blue.svg
+    :alt: Supports python3.5
+    :target: https://travis-ci.com/freieslabor/prepaid-mate
+
+.. |python3.6| image:: https://img.shields.io/badge/python-3.6-blue.svg
+    :alt: Supports python3.6
+    :target: https://travis-ci.com/freieslabor/prepaid-mate
+
+.. |python3.7| image:: https://img.shields.io/badge/python-3.7-blue.svg
+    :alt: Supports python3.7
+    :target: https://travis-ci.com/freieslabor/prepaid-mate
+
+.. |build-status| image:: https://travis-ci.com/freieslabor/prepaid-mate.svg?branch=master
+    :alt: Travis build status
+    :target: https://travis-ci.com/freieslabor/prepaid-mate
+
+.. |lgtm-alerts| image:: https://img.shields.io/lgtm/alerts/g/freieslabor/prepaid-mate.svg?logo=lgtm&logoWidth=18
+    :alt: Total lgtm alerts
+    :target: https://lgtm.com/projects/g/freieslabor/prepaid-mate/alerts/
+
+.. |lgtm-grade| image:: https://img.shields.io/lgtm/grade/python/g/freieslabor/prepaid-mate.svg?logo=lgtm&logoWidth=18
+    :alt: Language grade: Python
+    :target: https://lgtm.com/projects/g/freieslabor/prepaid-mate/context:python
---
+++ app.py
@@ -0,0 +1,361 @@
+#!/usr/bin/env python3
+"""Flask Prepaid Mate server"""
+
+import os
+import sqlite3
+import json
+from configparser import ConfigParser
+
+from flask import Flask, g, request
+from flask.logging import create_logger
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.exceptions import BadRequestKeyError
+
+CONF = ConfigParser()
+CONF_FILE = os.environ.get('CONFIG', './config')
+CONF.read_file(open(CONF_FILE))
+app = Flask(__name__)  # pylint: disable=invalid-name
+LOG = create_logger(app)
+
+def sql_integrity_error(exc):
+    """Extract useful info from """
+    assert isinstance(exc, sqlite3.IntegrityError)
+
+    unique_error_prefix = 'UNIQUE constraint failed: '
+    if exc.args[0].startswith(unique_error_prefix):
+        field = exc.args[0].replace(unique_error_prefix, '')
+        _, field = field.split('.', 1)
+        return '{} already exists'.format(field), 400
+    return 'Database integrity error', 400
+
+def get_db():
+    """
+    Helper to retrieve DB connection, taken from
+    http://flask.pocoo.org/docs/1.0/patterns/sqlite3/
+    """
+    database = getattr(g, '_database', None)
+    if database is None:
+        database = g._database = sqlite3.connect(CONF.get('DEFAULT', 'database'))
+    database.row_factory = sqlite3.Row
+    return database
+
+@app.teardown_appcontext
+def close_connection(_):
+    """
+    Helper to retrieve DB connection
+    http://flask.pocoo.org/docs/1.0/patterns/sqlite3/
+    """
+    database = getattr(g, '_database', None)
+    if database is not None:
+        database.close()
+
+def query_db(query, args=(), one=False):
+    """
+    Helper to query DB
+    http://flask.pocoo.org/docs/1.0/patterns/sqlite3/
+    """
+    cur = get_db().execute(query, args)
+    result = cur.fetchall()
+    cur.close()
+    return (result[0] if result else None) if one else result
+
+def password_check(req):
+    """
+    Helper to check username and password, taken from "name" and "password"
+    POST parameters.
+    """
+    try:
+        account = query_db('SELECT id, password_hash FROM accounts WHERE name = ?',
+                           [req.form['name']], one=True)
+        if 'password' not in req.form:
+            LOG.info('password check failed: no password given')
+            raise KeyError()
+    except KeyError:
+        LOG.info('password check failed: no username given')
+        raise KeyError('Incomplete request')
+
+    try:
+        account_id, password_hash = tuple(account)
+    except TypeError:
+        LOG.info('password check failed: no such account "%s" in database',
+                 req.form['name'])
+        raise TypeError('No such account in database')
+
+    if not check_password_hash(password_hash, req.form['password']):
+        LOG.info('password check failed: wrong password for account "%s"',
+                 req.form['name'])
+        raise ValueError('Wrong password')
+
+    return account_id
+
+@app.route('/api/account/create', methods=['POST'])
+def account_create():
+    """
+    Creates account with given parameters.
+
+    Expects POST parameters:
+    - name
+    - password
+    - code
+
+    Returns:
+    200 "ok"
+    400 with error message
+    500 on broken code
+    """
+    try:
+        drink_barcode = query_db('SELECT barcode FROM drinks WHERE barcode= ?',
+                                 [request.form['code']], one=True)
+        if drink_barcode is not None:
+            return 'This code is already used for a drink', 400
+
+        password_hash = generate_password_hash(request.form['password'])
+        query_db('INSERT INTO accounts (name, password_hash, barcode, saldo) VALUES (?, ?, ?, 0)',
+                 [request.form['name'], password_hash, request.form['code']])
+
+        if request.form['name'] == '' or request.form['password'] == '' \
+            or request.form['code'] == '':
+            get_db().rollback()
+            raise BadRequestKeyError
+
+        get_db().commit()
+        LOG.info('Account "%s (identifier: "%s") created',
+                 request.form['name'], request.form['code'])
+    except BadRequestKeyError:
+        exc_str = 'Incomplete request'
+        LOG.warning(exc_str)
+        return exc_str, 400
+    except sqlite3.IntegrityError as exc:
+        exc_str = sql_integrity_error(exc)
+        LOG.error(exc_str)
+        return exc_str
+    except sqlite3.OperationalError as exc:
+        LOG.error(exc)
+        return exc, 400
+
+    return 'ok'
+
+@app.route('/api/account/modify', methods=['POST'])
+def account_modify():
+    """
+    Modifies account identified by "name" and "password" with given parameters.
+
+    Expects POST parameters:
+    - name
+    - password
+    - new_name
+    - new_password
+    - new_code
+
+    Returns 200 "ok"
+    400 with error message
+    500 on broken code
+    """
+    try:
+        password_check(request)
+    except (KeyError, TypeError, ValueError) as exc:
+        return exc.args[0], 400
+
+    try:
+        new_password_hash = generate_password_hash(request.form['new_password'])
+        query_db('UPDATE accounts SET name=?, password_hash=?, barcode=? WHERE name=?',
+                 [request.form['new_name'], new_password_hash,
+                  request.form['new_code'], request.form['name']])
+
+        if request.form['new_name'] == '' or request.form['new_password'] == '' \
+            or request.form['new_code'] == '':
+            get_db().rollback()
+            raise BadRequestKeyError
+
+        get_db().commit()
+        LOG.info('Account "%s modified', request.form['name'])
+    except BadRequestKeyError:
+        exc_str = 'Incomplete request'
+        LOG.warning(exc_str)
+        return exc_str, 400
+    except sqlite3.IntegrityError as exc:
+        exc_str = sql_integrity_error(exc)
+        LOG.error(exc_str)
+        return exc_str, 400
+    except sqlite3.OperationalError as exc:
+        LOG.error(exc)
+        return exc, 400
+
+    return 'ok'
+
+@app.route('/api/account/view', methods=['POST'])
+def account_view():
+    """
+    Returns information on account identified by "name" and "password".
+
+    Expects POST parameters:
+    - name
+    - password
+
+    Returns 200 with json tuple (name, barcode, saldo)
+    400 with error message
+    500 on broken code
+    """
+    try:
+        password_check(request)
+    except (KeyError, TypeError, ValueError) as exc:
+        return exc.args[0], 400
+
+    account = query_db('SELECT name, barcode, saldo FROM accounts WHERE name = ?',
+                       [request.form['name']], one=True)
+
+    return json.dumps(tuple(account))
+
+@app.route('/api/money/add', methods=['POST'])
+def money_add():
+    """
+    Add "money" to account identified "name" and "password".
+
+    Expects POST parameters:
+    - name
+    - password
+    - money
+
+    Returns 200 "ok"
+    400 with error message
+    500 on broken code
+    """
+    try:
+        account_id = password_check(request)
+    except (KeyError, TypeError, ValueError) as exc:
+        return exc.args[0], 400
+
+    try:
+        try:
+            money = int(request.form['money'])
+        except ValueError:
+            LOG.info('Money for "%s" not given in cents', request.form['name'])
+            return 'Money must be specified in cents', 400
+
+        if money <= 0:
+            LOG.info('Negative amount of money given for account "%s"',
+                     request.form['name'])
+            return 'Zero/negative money given', 400
+
+        query_db('UPDATE accounts SET saldo=saldo+? WHERE id=?',
+                 [money, account_id])
+        query_db('INSERT INTO money_logs (account_id, amount, timestamp) VALUES (?, ?, strftime("%s", "now"))',  # pylint: disable=line-too-long
+                 [account_id, money])
+        get_db().commit()
+        LOG.info('Added %d cents to account "%s"', money, request.form['name'])
+    except Exception as exc:  # pylint: disable=broad-except
+        get_db().rollback()
+        if isinstance(exc, (BadRequestKeyError, KeyError)):
+            exc_str = 'Incomplete request'
+        elif isinstance(exc, sqlite3.IntegrityError):
+            exc_str = sql_integrity_error(exc)
+        else:
+            exc_str = str(exc)
+
+        LOG.error(exc_str)
+        return exc_str, 400
+
+    return 'ok'
+
+@app.route('/api/money/view', methods=['POST'])
+def money_view():
+    """
+    View transactions of account identified "name" and "password".
+
+    Expects POST parameters:
+    - name
+    - password
+
+    Returns 200 with json tuple (amount, transaction name, timestamp)
+    400 with error message
+    500 on broken code
+    """
+    try:
+        account_id = password_check(request)
+    except (KeyError, TypeError, ValueError) as exc:
+        return exc.args[0], 400
+
+    try:
+        transactions = query_db(
+            'SELECT 0-drinks.price as amount, drinks.name as name, pay_logs.timestamp as timestamp FROM pay_logs INNER JOIN drinks ON pay_logs.drink_id=drinks.id WHERE pay_logs.account_id=? UNION SELECT amount, ? as drink_name, timestamp FROM money_logs WHERE account_id=? ORDER BY timestamp DESC',  # pylint: disable=line-too-long
+            [account_id, 'Guthaben aufgeladen', account_id]
+        )
+    except BadRequestKeyError:
+        exc_str = 'Incomplete request'
+        LOG.warning(exc_str)
+        return exc_str, 400
+    except sqlite3.IntegrityError as exc:
+        exc_str = sql_integrity_error(exc)
+        LOG.error(exc_str)
+        return exc_str, 400
+
+    return json.dumps([tuple(row) for row in transactions])
+
+@app.route('/api/payment/perform', methods=['POST'])
+def payment_perform():
+    """
+    Perform payment transaction ("drink_barcode") on account identified by
+    "account_code". This is authorized on "superuserpassword".
+
+    Expects POST parameters:
+    - superuserpassword
+    - account_code
+    - drink_barcode
+
+    Returns 200 with json tuple (amount, transaction name, timestamp)
+    400 with error message
+    500 on broken code
+    """
+    if request.form['superuserpassword'] != \
+        CONF.get('DEFAULT', 'superuser-password'):
+        LOG.warning('Payment with wrong super user password')
+        return 'Wrong superuserpassword', 400
+
+    try:
+        account = query_db('SELECT id, saldo FROM accounts WHERE barcode=?',
+                           [request.form['account_code']], one=True)
+        try:
+            account_id, saldo = tuple(account)
+        except TypeError:
+            exc_str = 'Barcode does not belong to an account'
+            LOG.warning(exc_str)
+            return exc_str, 400
+
+        if account_id is None:
+            exc_str = 'No such account in database'
+            LOG.warning(exc_str)
+            return exc_str, 400
+
+        drink = query_db('SELECT id, price FROM drinks WHERE barcode=?',
+                         [request.form['drink_barcode']], one=True)
+        try:
+            drink_id, drink_price = tuple(drink)
+        except TypeError:
+            exc_str = 'No such drink in database'
+            LOG.warning(exc_str)
+            return exc_str, 400
+
+        if saldo - drink_price < 0:
+            exc_str = 'Insufficient funds'
+            LOG.warning(exc_str)
+            return exc_str, 400
+
+        query_db('INSERT INTO pay_logs (account_id, drink_id, timestamp) VALUES (?, ?, strftime("%s", "now"))',  # pylint: disable=line-too-long
+                 [account_id, drink_id])
+        query_db('UPDATE accounts SET saldo=saldo-? WHERE id=?',
+                 [drink_price, account_id])
+        get_db().commit()
+        LOG.warning('Account ID "%s" ordered %s (%d cents), new saldo=%d cents',
+                    account_id, drink_id, drink_price, saldo)
+        return str(saldo - drink_price)
+    except Exception as exc:  # pylint: disable=broad-except
+        get_db().rollback()
+        if isinstance(exc, BadRequestKeyError):
+            exc_str = 'Incomplete request'
+        elif isinstance(exc, sqlite3.IntegrityError):
+            exc_str = sql_integrity_error(exc)
+        else:
+            exc_str = str(exc)
+
+        LOG.warning(exc_str)
+        return exc_str, 400
