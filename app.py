2f3701b9d72eb370be264631efb198080fcb7af8
Bastian Stender <basti@randomprojects.de>
2019-01-25 20:31:52 +0100

    app: do not select all cols on password_check
    Signed-off-by: Bastian Stender <basti@randomprojects.de>

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def query_db(query, args=(), one=False):
 
 def password_check(request):
     try:
-        user = query_db('SELECT * FROM accounts WHERE name = ?',
+        user = query_db('SELECT id FROM accounts WHERE name = ?',
                         [request.form['name']], one=True)
     except KeyError:
         return "incomplete request", 400
