205bd70fca4dc57e44521b642b7b3d0a9097fde9
Bastian Stender <basti@randomprojects.de>
2019-01-25 18:50:01 +0100

    fixup! WIP: app: implemented account and money add

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ def account_modify():
     try:
         new_password_hash = generate_password_hash(request.form['new_password'])
         test = query_db('UPDATE accounts SET name=?, password_hash=?, barcode=? WHERE name=?',
-                 [request.form['new_name'], new_password_hash,
-                  request.form['barcode'], request.form['name']])
+                        [request.form['new_name'], new_password_hash,
+                         request.form['barcode'], request.form['name']])
         get_db().commit()
     except BadRequestKeyError:
         return "Incomplete request", 400
