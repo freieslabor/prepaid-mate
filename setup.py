29f94dad4270739c5a58f455de45fd70f16de880
Bastian Stender <basti@randomprojects.de>
2019-03-09 23:19:11 +0100

    setup: add basic setup.py, setup.cfg
    Signed-off-by: Bastian Stender <basti@randomprojects.de>

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__name__ == '__main__' and setup(name='prepaid-mate',
+    version='0.0',
+    author='Bastian Krause, Bayden',
+    url='https://github.com/freieslabor/prepaid-mate/',
+    author_email='basti@freieslabor.org',
+    install_requires=[
+        'requests',
+        'evdev',
+        'Flask'
+    ],
+    python_requires='>=3.5',
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'scanner-client = prepaid_mate.scanner_client:main',
+        ]
+    })
