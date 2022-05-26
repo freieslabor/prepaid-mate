9ad0766eb2badfc3e116d70ce36d1623afb72138
Bastian Krause <basti@randomprojects.de>
2022-05-26 21:10:53 +0200

    tests: client: improve test
    Move the helper function out of the test, so future tests can also use
    it. We can no longer rely on global variables there, so pass the
    process.
    
    Also drop some hard-coded values in favor of variables we can use to get
    their value.
    
    Signed-off-by: Bastian Krause <basti@randomprojects.de>

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,25 +2,29 @@
 # disable unused arguments used to run fixtures
 # pylint: disable=unused-argument
 
+import re
+
 import pytest
 
-def test_client(flask_server, create_account_with_balance, scanner_client):
-    """Test payment on scanner_client.py with flask server and preset account."""
-    import re
+def get_log_line(proc, logger='.*:root:'):
+    for line in iter(proc.stderr.readline, ''):
+        line = line.decode('utf-8')
+        if re.search(logger, line):
+            return line
 
-    create_account_with_balance(150)
+    return None
+
+
+def test_client_payment(flask_server, create_account_with_balance, create_drink, scanner_client):
+    """Test payment on scanner_client.py with flask server and preset account."""
+    data = create_account_with_balance(150)
+    drink = create_drink
     proc = scanner_client()
 
-    def get_log_line(logger='.*:root:'):
-        for line in iter(proc.stderr.readline, ''):
-            line = line.decode('utf-8')
-            if re.search(logger, line):
-                return line
+    assert 'account code: {}'.format(data['code']) in get_log_line(proc)
+    get_log_line(proc) # ignore user greeting
 
-        return None
-
-    assert 'account code: 0016027465' in get_log_line()
-    get_log_line() # ignore user greeting
-    assert 'account "0016027465" ordered "42254300"'  in get_log_line()
-    assert 'calling API with' in get_log_line()
-    assert 'callback successful: ' in get_log_line()
+    # note that drink['barcode']) is in the umockdev recorded session being played back
+    assert 'account "{}" ordered "{}"'.format(data['code'], drink['barcode']) in get_log_line(proc)
+    assert 'calling API with' in get_log_line(proc)
+    assert 'callback successful: ' in get_log_line(proc)
